4919a55bd56e5efdb8cd1df5f5bae0105b14d076
uriapete <uriarteangelo@gmail.com>
2023-03-18 13:40:00 -0700

    Created show route

--- controllers/drinks.ts
+++ controllers/drinks.ts
@@ -6,8 +6,15 @@ const router = express.Router();
 const drinkModel = require("../models/Drinks")
 const drinks = drinkModel.drinks;
 
+// index route
 router.get("/drinks", (req: Request, res: Response) => {
     res.render("drinks_index", {drinks});
+})
+
+// show route
+router.get("/drinks/:drinkID", (req: Request, res: Response) => {
+    res.send(req.params.drinkID)
+    // res.render("drinks_index", {drinks});
 })
 
 module.exports = router;
