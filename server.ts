792e51ed5ac5d3fcf6111b49f3f441ee9340e11b
uriapete <uriarteangelo@gmail.com>
2023-03-18 12:37:31 -0700

    Included drinks database and set up /drinks route

--- server.ts
+++ server.ts
@@ -3,10 +3,17 @@ import { Request, Response } from "express";
 const express = require("express");
 const app = express();
 
+const drinkModel = require("./models/drinks");
+const drinks = drinkModel.drinks;
+
 const PORT:number = 4000;
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("Welcome to the GitPub App!");
+})
+
+app.get("/drinks",(req:Request,res:Response)=>{
+    res.send(drinks);
 })
 
 app.listen(PORT,()=>{
