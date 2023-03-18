b0a924e43d270501db9352f78c4973c50ebaf6e3
uriapete <uriarteangelo@gmail.com>
2023-03-18 12:56:48 -0700

    Now using static public directory, set up rendering for drinks_index

--- server.ts
+++ server.ts
@@ -3,8 +3,14 @@ import { Request, Response } from "express";
 const express = require("express");
 const app = express();
 
+const ejs = require("ejs");
+
 const drinkModel = require("./models/drinks");
 const drinks = drinkModel.drinks;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 const PORT:number = 4000;
 
@@ -13,7 +19,7 @@ app.get("/",(req:Request,res:Response)=>{
 })
 
 app.get("/drinks",(req:Request,res:Response)=>{
-    res.send(drinks);
+    res.render("drinks_index");
 })
 
 app.listen(PORT,()=>{
