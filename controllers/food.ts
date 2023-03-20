3a816485dc0444bda9dc4d8fa220beca58cfaf7f
uriapete <uriarteangelo@gmail.com>
2023-03-20 14:15:25 -0700

    Created food controller

---
+++ controllers/food.ts
@@ -0,0 +1,19 @@
+import { Request,Response } from "express";
+
+const express = require("express");
+const router = express.Router();
+
+const foodModel = require("../models/food");
+const food = foodModel.food;
+
+// index route
+router.get("/food", (req: Request, res: Response) => {
+    res.render("food_index", {food});
+})
+
+// show route
+router.get("/food/:foodID", (req: Request, res: Response) => {
+    res.render("food_show", {food : food[parseInt(req.params.foodID)]});
+})
+
+module.exports = router;
